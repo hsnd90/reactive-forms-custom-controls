852871295fc5937bac9322f82b5ff7df29ff016a
Hasan <44682484+hsnd90@users.noreply.github.com>
2023-09-24 23:06:43 +0300

    some fixes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
-import { MyDropdownModule } from "./demo/my-dropdown.module";
+import { MyDropdownModule } from "./my-dropdown/my-dropdown.module";
 import { MyOverlayDropdownModule } from "./my-overlay-dropdown/my-overlay-dropdown.module";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
